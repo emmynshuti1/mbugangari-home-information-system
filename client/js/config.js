441e60e06a741d43eaec11ae1ea3c3e11d6f7654
nshutiemmy1 <nshutiemmanuel860@gmail.com>
2026-07-24 14:09:37 +0200

    chc

--- client/js/config.js
+++ client/js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    API_URL: "https://mbugangari-home-api.onrender.com//api",
-    IMAGE_URL: "https://mbugangari-home-api.onrender.com//uploads"
+    API_URL: "https://mbugangari-home-api.onrender.com/api",
+    IMAGE_URL: "https://mbugangari-home-api.onrender.com/uploads"
 };
