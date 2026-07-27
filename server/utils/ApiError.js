7c3417fac58ee977862524931785de51ffe86e49
nshutiemmy1 <nshutiemmanuel860@gmail.com>
2026-07-27 13:18:58 +0200

    chc

--- server/utils/ApiError.js
+++ server/utils/ApiError.js
@@ -1,21 +1,14 @@
 class ApiError extends Error {
 
     constructor(statusCode, message) {
-
         super(message);
 
         this.statusCode = statusCode;
+        this.success = false;
 
+        Error.captureStackTrace(this, this.constructor);
     }
 
-}
-
-throw new ApiError(
-
-    404,
-
-    "House not found"
-
-);
+} 
 
 module.exports = ApiError;
--- server/utils/ApiResponse.js
+++ server/utils/ApiResponse.js
@@ -1,27 +1,11 @@
 class ApiResponse {
 
     constructor(success, message, data = null) {
-
         this.success = success;
         this.message = message;
         this.data = data;
-
     }
 
 }
-
-res.json(
-
-    new ApiResponse(
-
-        true,
-
-        "House Created",
-
-        house
-
-    )
-
-);
 
 module.exports = ApiResponse;
