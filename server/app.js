2d59f44ae10bdd79b587574ba68af83e015bc49e
nshutiemmy1 <nshutiemmanuel860@gmail.com>
2026-07-28 10:57:03 +0200

    chc

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ require("./config/db");
 
 app.use(cors({
     origin: [
-        "http://localhost:5000",
+        "http://localhost:5000", 
         "http://127.0.0.1:5000",
         "https://mbugangari-home-api.onrender.com"
     ],
