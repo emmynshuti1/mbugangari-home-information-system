7f7150a5ea41715e7c97fbbad43984d0e1edeb19
nshutiemmy1 <nshutiemmanuel860@gmail.com>
2026-07-29 15:57:59 +0200

    chc

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ app.use(cors({
     origin: [
         "http://localhost:5000", 
         "http://127.0.0.1:5000",
-        "https://mbugangari-home-information-system.vercel.app/"
+        "https://mbugangari-home-api.onrender.com"
     ],
     credentials: true
 }));
