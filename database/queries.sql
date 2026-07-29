40e1a7c25b20a0878b4ebe31b9bd03a42db1e1fb
nshutiemmy1 <nshutiemmanuel860@gmail.com>
2026-07-29 20:07:50 +0200

    chc

---
+++ database/queries.sql
@@ -0,0 +1,13 @@
+SELECT * FROM houses;
+
+SELECT * FROM rooms;
+
+SELECT * FROM gallery;
+
+SELECT * FROM materials;
+
+SELECT * FROM history;
+
+SELECT * FROM nearby_places;
+
+SELECT * FROM administrators;
